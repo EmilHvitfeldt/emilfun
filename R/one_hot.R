f2c0f176b51d7825fb5c5dab2a523ddc105f4aca
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2017-12-09 10:27:12 -0800

    adding one_hot function

---
+++ R/one_hot.R
@@ -0,0 +1,26 @@
+#' Transform a catagorical variable to one hot representation
+#'
+#' @param data a data.frame or tibble.
+#' @param var name of catagorical variable.
+#' @return A data.frame or tibble with the catagorical variable replaced with
+#'  an one hot representation.
+#' @examples
+#' one_hot(iris, Species)
+#' @export
+one_hot <- function(data, var) {
+
+  var_enquo <- enquo(var)
+  items <- data %>% pull(!!var_enquo)
+  items_unique <- items %>% unique()
+
+  out <- matrix(0, NROW(data), length(items_unique))
+  colnames(out) <- items_unique
+
+  for (i in items_unique) {
+    out[, i] <- items == i
+  }
+
+  data %>%
+    select(-!!var_enquo) %>%
+    bind_cols(as.tibble(out))
+}
