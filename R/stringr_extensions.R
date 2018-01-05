8332b3fb93f6af2bf38e5f8d15ce34ccb48649bc
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2018-01-04 16:41:56 -0800

    adding str_between_n

--- R/stringr_extensions.R
+++ R/stringr_extensions.R
@@ -27,6 +27,22 @@ str_between_all <- function(string, start, end) {
     purrr::map(~ .x %>% str_between(start, end))
 }
 
+#' Extract nth text between two patterns (not including)
+#'
+#' @param string A number. Input vector.
+#' @param start Pattern to match after.
+#' @param end Pattern to match before.
+#' @param n Integer.
+#' @return A character vector.
+#' @export
+#'
+str_between_n <- function (string, start, end, n) {
+  stringr::str_extract_all(string,
+                           stringr::str_c(start, "(.*?)", end, collapse = "")) %>%
+    purrr::map(~.x %>% str_between(start, end) %>% .[n]) %>%
+    unlist()
+}
+
 #' Extract text before pattern (not including)
 #'
 #' @param string A number. Input vector.
