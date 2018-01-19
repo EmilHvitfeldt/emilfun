15f8309b0269276d43843a6fa2641a8abff69168
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2018-01-19 11:28:01 -0800

    tidyfy arg order

--- R/kmeans_df.R
+++ R/kmeans_df.R
@@ -3,15 +3,15 @@
 #' This function is a simple wrapper that lets you work with \code{kmeans()}
 #' while using data.frame, thus allowing you to stay with the same format.
 #'
+#' @param data A data.frame.
 #' @param formula an object of class \code{"\link{formula}"} (or one that
 #' can be coerced to that class)
-#' @param data A data.frame.
 #' @param \dots Additional arguments to be passed to the kmeans function.
 #' @return an object of class "kmeans".
 #' @examples
 #' kmeans_df(~., cars, centers = 2)
 #' @export
-kmeans_df <- function(formula, data, ...) {
+kmeans_df <- function(data, formula, ...) {
   data_mat <- as.matrix(model.frame(formula, data))
 
   kmeans(x = data_mat, ...)
