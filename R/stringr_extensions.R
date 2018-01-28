98c998762da46358651e7b81aa9c77a1c52f0314
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2018-01-27 21:23:40 -0800

    added more stringr extensions and fixed documentation

--- R/stringr_extensions.R
+++ R/stringr_extensions.R
@@ -1,6 +1,6 @@
 #' Extract first text between two patterns (not including)
 #'
-#' @param string A number. Input vector.
+#' @param string A character vector. Input vector.
 #' @param start Pattern to match after.
 #' @param end Pattern to match before.
 #' @return A character vector.
@@ -15,7 +15,7 @@ str_between <- function(string, start, end) {
 
 #' Extract all text between two patterns (not including)
 #'
-#' @param string A number. Input vector.
+#' @param string A character vector. Input vector.
 #' @param start Pattern to match after.
 #' @param end Pattern to match before.
 #' @return A character vector.
@@ -29,7 +29,7 @@ str_between_all <- function(string, start, end) {
 
 #' Extract nth text between two patterns (not including)
 #'
-#' @param string A number. Input vector.
+#' @param string A character vector. Input vector.
 #' @param start Pattern to match after.
 #' @param end Pattern to match before.
 #' @param n Integer.
@@ -45,7 +45,7 @@ str_between_n <- function (string, start, end, n) {
 
 #' Extract text before pattern (not including)
 #'
-#' @param string A number. Input vector.
+#' @param string A character vector. Input vector.
 #' @param pattern Pattern to match before.
 #' @return A character vector.
 #' @export
@@ -56,7 +56,7 @@ str_before <- function(string, pattern) {
 
 #' Extract text after pattern (not including)
 #'
-#' @param string A number. Input vector.
+#' @param string A character vector. Input vector.
 #' @param pattern Pattern to match after.
 #' @return A character vector.
 #' @export
@@ -64,3 +64,45 @@ str_before <- function(string, pattern) {
 str_after <- function(string, pattern) {
   stringr::str_extract(string, stringr::str_c("(?<=", pattern, ").*$"))
 }
+
+#' Extract the nth word
+#'
+#' Fairly rough function. Ideal to work with n-grams.
+#'
+#' @param string A character vector. Input vector.
+#' @param n Integer. Which word to extract.
+#' @param sep Pattern to seperate words with.
+#' @return A character vector.
+#' @export
+#'
+str_nth_word <- function(string, n, sep = " ") {
+  stringr::str_split(string, pattern = " ") %>%
+    purrr::map_chr(~ .x[n])
+}
+
+#' Extract after last
+#'
+#' @param string A character vector. Input vector.
+#' @param pattern Pattern to look after.
+#' @return A character vector.
+#' @export
+#'
+str_after_last <- function(string, pattern) {
+  stringr::str_extract(string,
+                       stringr::str_c("([^", pattern, "]+$)", collapse = ""))
+}
+
+#' Before first
+#'
+#' @param string A character vector. Input vector.
+#' @param pattern Pattern to look after.
+#' @return A character vector.
+#' @export
+#'
+str_before_first <- function(string, pattern) {
+  stringr::str_extract(string,
+                       stringr::str_c("^(.+?)", pattern, collapse = ""))
+}
+
+
+
