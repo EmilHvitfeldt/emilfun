08bdf734dc96b0af3a6d33f2b172aba38d2fd0e0
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2018-02-03 21:22:59 -0800

    adding rztpois function

---
+++ R/ztpois.R
@@ -0,0 +1,26 @@
+#' The Zero-truncated Poisson distribution
+#'
+#' Random generation for the Poisson distribution with parameter lambda.
+#'
+#' @param n number of random values to return.
+#' @param lambda vector of (non-negative) means.
+#' @return Generates random deviates of the zero truncated Poisson.
+#' @examples
+#' rztpois(100)
+#' rztpois(1000, 5)
+#' @export
+rztpois <- function(n, lambda = 1) {
+  res <- integer()
+  n_sample <- n
+
+  # n = 0
+  pois_sample <- stats::rpois(n = n_sample, lambda = lambda)
+  res <- c(res, pois_sample[pois_sample != 0])
+  n_sample <- (n - length(res)) / mean(pois_sample != 0)
+
+  while(length(res) <= n) {
+    pois_sample <- stats::rpois(n = n_sample, lambda = lambda)
+    res <- c(res, pois_sample[pois_sample != 0])
+  }
+  res[seq_len(n)]
+}
